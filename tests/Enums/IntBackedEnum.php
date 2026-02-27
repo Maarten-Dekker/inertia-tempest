6548feea0c11434c2d98454d4f9d87e75f3dde73
Maarten Dekker <65236304+Maarten-Dekker@users.noreply.github.com>
2026-02-27 12:54:56 +0100

    feat!: add enum support in `Inertia::render()` (#40)

---
+++ tests/Enums/IntBackedEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inertia\Tests\Enums;
+
+enum IntBackedEnum: int
+{
+    case Zero = 0;
+}
---
+++ tests/Enums/StringBackedEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inertia\Tests\Enums;
+
+enum StringBackedEnum: string
+{
+    case UsersIndex = 'UsersPage/Index';
+}
