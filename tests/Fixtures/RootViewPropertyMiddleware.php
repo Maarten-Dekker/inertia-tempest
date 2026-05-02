f28b71454b39a6b04b704a711d73d59d7662697c
Maarten Dekker <maarten.d@live.nl>
2026-05-02 12:14:38 +0200

    fix

--- tests/Fixtures/RootViewPropertyMiddleware.php
+++ tests/Fixtures/RootViewPropertyMiddleware.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Inertia\Tests\Fixtures;
 
-use Override;
 use Inertia\Middleware\Middleware;
+use Override;
 use Tempest\Discovery\SkipDiscovery;
 
 #[SkipDiscovery]
