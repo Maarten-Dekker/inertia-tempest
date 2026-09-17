d3bbdcefdbb76a74e19f7e6f387f3a7ac6d848cd
Maarten Dekker <maarten.d@live.nl>
2026-09-17 13:05:01 +0200

    restore throw‑on‑error

--- src/Views/InertiaView.php
+++ src/Views/InertiaView.php
@@ -33,7 +33,7 @@ final class InertiaView implements View
     {
         $id = trim($id) === '' ? 'app' : $id;
 
-        $json = json_encode($this->inertia['page'], JSON_HEX_TAG);
+        $json = json_encode($this->inertia['page'], JSON_HEX_TAG | JSON_THROW_ON_ERROR);
 
         if ($this->ssrBody) {
             $body = $this->ssrBody;
